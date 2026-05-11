02d7240ba5784c3cb2831f6a6e5631e920855ed7
anastasiia-i-andreiko <anastasiia.i.andreiko@ukd.edu.ua>
2026-05-11 12:37:35 +0300

    Update session.py

--- database/session.py
+++ database/session.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.base import Base
-# Імпортуємо моделі, щоб Base знав, які таблиці створювати
-from database.models import Expense, Goal, GoalHistory 
+# Обов'язково імпортуємо моделі тут
+from database.models import Expense, Goal, GoalHistory
 
 engine = create_engine('sqlite:///spendly.db')
 
-# Ця команда створює таблиці в базі даних автоматично
+# Цей рядок автоматично створює таблиці, якщо їх немає в файлі .db
 Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
