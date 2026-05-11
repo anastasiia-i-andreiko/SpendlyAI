ae8b333af2d500ecaadf9904281a4500a77f97bb
anastasiia-i-andreiko <anastasiia.i.andreiko@ukd.edu.ua>
2026-05-11 13:09:10 +0300

    Update session.py

--- database/session.py
+++ database/session.py
@@ -2,7 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.base import Base
 
-# Створюємо двигун бази даних
 engine = create_engine("sqlite:///./spendly.db", connect_args={"check_same_thread": False})
-# Створюємо фабрику сесій
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
